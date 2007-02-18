6eb1202254829004e8d222dbef3933b6b34db70b
Frank Bille <frank@teachus.dk>
2007-02-18 22:21:11 +0000

    Make the sending of email run in a different thread. This should hopefully make the application seem faster when the mail server is slow.
    git-svn-id: http://svn.teachus.dk/dk.frankbille.teachus/trunk@34 9534b298-6716-0410-9c14-e4ca5a2019ab

--- src/main/java/dk/frankbille/teachus/bean/impl/MailBeanImpl.java
+++ src/main/java/dk/frankbille/teachus/bean/impl/MailBeanImpl.java
@@ -34,85 +34,93 @@ public class MailBeanImpl implements MailBean {
 	}
 
 	public void sendWelcomeMail(final Pupil pupil) {
-		mailSender.send(new MimeMessagePreparator() {
-			public void prepare(MimeMessage mimeMessage) throws Exception {
-				MimeMessageHelper message = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+		new Thread(new Runnable() {
+			public void run() {
+				mailSender.send(new MimeMessagePreparator() {
+					public void prepare(MimeMessage mimeMessage) throws Exception {
+						MimeMessageHelper message = new MimeMessageHelper(mimeMessage, false, "UTF-8");
 
-				// Sender and recipient
-				String email = pupil.getTeacher().getEmail();
-				String fromName = pupil.getTeacher().getName();
-				
-				message.setFrom(new InternetAddress(email, fromName));
-				message.setTo(new InternetAddress(pupil.getEmail(), pupil.getName()));
-				
-				// Parse the template
-				Map<String, Object> model = new HashMap<String, Object>();
-				model.put("server", "http://teachus.billen.dk/");
-				model.put("from", HtmlUtils.htmlEscape(fromName));
-				model.put("name", HtmlUtils.htmlEscape(pupil.getName()));
-				model.put("username", HtmlUtils.htmlEscape(pupil.getUsername()));
-				model.put("password", HtmlUtils.htmlEscape(pupil.getPassword()));
-				String template = velocityBean.mergeTemplate("dk/frankbille/teachus/bean/impl/WelcomeMail", model, pupil.getTeacher().getLocale());
-				
-				// Subject
-				// First line in parsed template is the subject
-				String subject = template.substring(0, template.indexOf('\n'));
-				message.setSubject(subject);
-				
-				// Text
-				String text = template.substring(template.indexOf('\n'));
-				message.setText(text, true);
+						// Sender and recipient
+						String email = pupil.getTeacher().getEmail();
+						String fromName = pupil.getTeacher().getName();
+						
+						message.setFrom(new InternetAddress(email, fromName));
+						message.setTo(new InternetAddress(pupil.getEmail(), pupil.getName()));
+						
+						// Parse the template
+						Map<String, Object> model = new HashMap<String, Object>();
+						model.put("server", "http://teachus.billen.dk/");
+						model.put("from", HtmlUtils.htmlEscape(fromName));
+						model.put("name", HtmlUtils.htmlEscape(pupil.getName()));
+						model.put("username", HtmlUtils.htmlEscape(pupil.getUsername()));
+						model.put("password", HtmlUtils.htmlEscape(pupil.getPassword()));
+						String template = velocityBean.mergeTemplate("dk/frankbille/teachus/bean/impl/WelcomeMail", model, pupil.getTeacher().getLocale());
+						
+						// Subject
+						// First line in parsed template is the subject
+						String subject = template.substring(0, template.indexOf('\n'));
+						message.setSubject(subject);
+						
+						// Text
+						String text = template.substring(template.indexOf('\n'));
+						message.setText(text, true);
 
-				mimeMessage.addHeader("X-Mailer", "TeachUs ("+ApplicationUtils.getVersion()+")");
-			}
-		});
+						mimeMessage.addHeader("X-Mailer", "TeachUs ("+ApplicationUtils.getVersion()+")");
+					}
+				});
+			}			
+		}).start();		
 	}
 	
 	public void sendNewBookingsMail(final Teacher teacher, final List<PupilBooking> pupilBookings) {
 		if (pupilBookings.isEmpty() == false) {
-			mailSender.send(new MimeMessagePreparator() {
-				public void prepare(MimeMessage mimeMessage) throws Exception {
-					MimeMessageHelper message = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-	
-					// Sender and recipient
-					String fromEmail = "teachus@billen.dk";
-					String fromName = "TeachUs Booking System";
-					
-					message.setFrom(new InternetAddress(fromEmail, fromName));
-					message.setTo(new InternetAddress(teacher.getEmail(), teacher.getName()));
-					
-					Locale locale = teacher.getLocale();
-					
-					// Build up bookingslist and format date
-					List<FormattedPupilBooking> pupilBookingList = new ArrayList<FormattedPupilBooking>();
-					for (PupilBooking pupilBooking : pupilBookings) {
-						FormattedPupilBooking formattedPupilBooking = new FormattedPupilBooking();
-						formattedPupilBooking.setPupilBooking(pupilBooking);
-						SimpleDateFormat dateFormat = new SimpleDateFormat("EE, d. MMM yyyy H:mm", locale);
-						formattedPupilBooking.setFormattedDate(dateFormat.format(pupilBooking.getDate()));
-						pupilBookingList.add(formattedPupilBooking);
-					}
-					
-					// Parse the template
-					Map<String, Object> model = new HashMap<String, Object>();
-					model.put("from", HtmlUtils.htmlEscape(fromName));
-					model.put("pupilBookingList", pupilBookingList);
-					model.put("pupilHeader", "Sanger"); // TODO externalize
-					model.put("dateHeader", "Dato/Tidspunkt"); // TODO externalize
-					String template = velocityBean.mergeTemplate("dk/frankbille/teachus/bean/impl/NewBookingsMail", model, locale);
-					
-					// Subject
-					// First line in parsed template is the subject
-					String subject = template.substring(0, template.indexOf('\n'));
-					message.setSubject(subject);
-					
-					// Text
-					String text = template.substring(template.indexOf('\n'));
-					message.setText(text, true);
-	
-					mimeMessage.addHeader("X-Mailer", "TeachUs ("+ApplicationUtils.getVersion()+")");
-				}
-			});
+			new Thread(new Runnable() {
+				public void run() {
+					mailSender.send(new MimeMessagePreparator() {
+						public void prepare(MimeMessage mimeMessage) throws Exception {
+							MimeMessageHelper message = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+			
+							// Sender and recipient
+							String fromEmail = "teachus@billen.dk";
+							String fromName = "TeachUs Booking System";
+							
+							message.setFrom(new InternetAddress(fromEmail, fromName));
+							message.setTo(new InternetAddress(teacher.getEmail(), teacher.getName()));
+							
+							Locale locale = teacher.getLocale();
+							
+							// Build up bookingslist and format date
+							List<FormattedPupilBooking> pupilBookingList = new ArrayList<FormattedPupilBooking>();
+							for (PupilBooking pupilBooking : pupilBookings) {
+								FormattedPupilBooking formattedPupilBooking = new FormattedPupilBooking();
+								formattedPupilBooking.setPupilBooking(pupilBooking);
+								SimpleDateFormat dateFormat = new SimpleDateFormat("EE, d. MMM yyyy H:mm", locale);
+								formattedPupilBooking.setFormattedDate(dateFormat.format(pupilBooking.getDate()));
+								pupilBookingList.add(formattedPupilBooking);
+							}
+							
+							// Parse the template
+							Map<String, Object> model = new HashMap<String, Object>();
+							model.put("from", HtmlUtils.htmlEscape(fromName));
+							model.put("pupilBookingList", pupilBookingList);
+							model.put("pupilHeader", "Sanger"); // TODO externalize
+							model.put("dateHeader", "Dato/Tidspunkt"); // TODO externalize
+							String template = velocityBean.mergeTemplate("dk/frankbille/teachus/bean/impl/NewBookingsMail", model, locale);
+							
+							// Subject
+							// First line in parsed template is the subject
+							String subject = template.substring(0, template.indexOf('\n'));
+							message.setSubject(subject);
+							
+							// Text
+							String text = template.substring(template.indexOf('\n'));
+							message.setText(text, true);
+			
+							mimeMessage.addHeader("X-Mailer", "TeachUs ("+ApplicationUtils.getVersion()+")");
+						}
+					});
+				}			
+			}).start();	
 		}
 	}
 	
