66ac3f3ae12efceb2f77cdf29e931963e4490138
Frank Bille <frank@teachus.dk>
2007-02-19 18:56:53 +0000

    Allow for no periods in the calendar. Display a warning message if that's the case.
    git-svn-id: http://svn.teachus.dk/dk.frankbille.teachus/trunk@40 9534b298-6716-0410-9c14-e4ca5a2019ab

--- src/main/java/dk/frankbille/teachus/frontend/components/CalendarPanel.java
+++ src/main/java/dk/frankbille/teachus/frontend/components/CalendarPanel.java
@@ -18,87 +18,112 @@ import wicket.markup.repeater.RepeatingView;
 import dk.frankbille.teachus.domain.DatePeriod;
 import dk.frankbille.teachus.domain.Period;
 import dk.frankbille.teachus.domain.Periods;
+import dk.frankbille.teachus.frontend.TeachUsSession;
 import dk.frankbille.teachus.frontend.utils.Formatters;
 import dk.frankbille.teachus.frontend.utils.Icons;
 
 public abstract class CalendarPanel extends Panel {
 	private static final long serialVersionUID = 1L;
 	
-	public CalendarPanel(String wicketId, DateMidnight pageDate, Periods periods) {
+	public CalendarPanel(String wicketId, DateMidnight pageDate, final Periods periods) {
 		super(wicketId);
 		
-		DateMidnight weekDate = pageDate.withDayOfWeek(DateTimeConstants.MONDAY);
-		final DateMidnight firstWeekDate = weekDate;
-		List<DatePeriod> dates = new ArrayList<DatePeriod>();
-		List<DatePeriod> weekDates = periods.generateDatesForWeek(weekDate.toDate());
-		do {
-			dates.addAll(weekDates);
-			weekDate = weekDate.plusWeeks(1);
-			weekDates = periods.generateDatesForWeek(weekDate.toDate());
-		} while(dates.size()+weekDates.size() <= 7);
+		final boolean showCalendar = periods.getPeriods().isEmpty() == false;
 		
+		Label statusMessage = new Label("statusMessage", TeachUsSession.get().getString("General.noPeriodsConfigured")) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isVisible() {
+				return showCalendar == false;
+			}
+		};
+		add(statusMessage);
 		
-		// Weeks
-		RepeatingView weeks = new RepeatingView("weeks"); //$NON-NLS-1$
-		add(weeks);
+		WebMarkupContainer calendar = new WebMarkupContainer("calendar") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isVisible() {
+				return showCalendar;
+			}
+		};
+		add(calendar);
 		
-		int w = -1;
-		RepeatingView days = null;
-		DateTimeFormatter formatWeekOfYear = Formatters.getFormatWeekOfYear();
-		Label weekHeader = null;
-		int daysInWeek = 0;
-		for (DatePeriod datePeriod : dates) {
-			Date date = datePeriod.getDate();
-			DateMidnight dm = new DateMidnight(date);
+		if (showCalendar) {
+			DateMidnight weekDate = pageDate.withDayOfWeek(DateTimeConstants.MONDAY);
+			final DateMidnight firstWeekDate = weekDate;
+			List<DatePeriod> dates = new ArrayList<DatePeriod>();
+			List<DatePeriod> weekDates = periods.generateDatesForWeek(weekDate.toDate());
+			do {
+				dates.addAll(weekDates);
+				weekDate = weekDate.plusWeeks(1);
+				weekDates = periods.generateDatesForWeek(weekDate.toDate());
+			} while(dates.size()+weekDates.size() <= 7);
 			
-			if (dm.getWeekOfWeekyear() != w) {
-				if (weekHeader != null) {
-					weekHeader.add(new SimpleAttributeModifier("colspan", ""+daysInWeek)); //$NON-NLS-1$ //$NON-NLS-2$
+			
+			// Weeks
+			RepeatingView weeks = new RepeatingView("weeks"); //$NON-NLS-1$
+			calendar.add(weeks);
+			
+			int w = -1;
+			RepeatingView days = null;
+			DateTimeFormatter formatWeekOfYear = Formatters.getFormatWeekOfYear();
+			Label weekHeader = null;
+			int daysInWeek = 0;
+			for (DatePeriod datePeriod : dates) {
+				Date date = datePeriod.getDate();
+				DateMidnight dm = new DateMidnight(date);
+				
+				if (dm.getWeekOfWeekyear() != w) {
+					if (weekHeader != null) {
+						weekHeader.add(new SimpleAttributeModifier("colspan", ""+daysInWeek)); //$NON-NLS-1$ //$NON-NLS-2$
+					}
+					daysInWeek = 0;
+					
+					WebMarkupContainer week = new WebMarkupContainer(weeks.newChildId());
+					weeks.add(week);
+					
+					weekHeader = new Label("weekHeader", formatWeekOfYear.print(dm)); //$NON-NLS-1$
+					week.add(weekHeader);
+					
+					days = new RepeatingView("days"); //$NON-NLS-1$
+					week.add(days);
+					
+					w = dm.getWeekOfWeekyear();
 				}
-				daysInWeek = 0;
 				
-				WebMarkupContainer week = new WebMarkupContainer(weeks.newChildId());
-				weeks.add(week);
+				WebMarkupContainer day = new WebMarkupContainer(days.newChildId());
+				days.add(day);
 				
-				weekHeader = new Label("weekHeader", formatWeekOfYear.print(dm)); //$NON-NLS-1$
-				week.add(weekHeader);
+				// Periods
+				RepeatingView periodsView = new RepeatingView("periods"); //$NON-NLS-1$
+				day.add(periodsView);
 				
-				days = new RepeatingView("days"); //$NON-NLS-1$
-				week.add(days);
+				for (Period period : datePeriod.getPeriods()) {
+					WebMarkupContainer periodCell = new WebMarkupContainer(periodsView.newChildId());
+					periodsView.add(periodCell);
+					
+					periodCell.add(createPeriodDateComponent("period", period, date));
+				}
 				
-				w = dm.getWeekOfWeekyear();
+				daysInWeek++;
 			}
 			
-			WebMarkupContainer day = new WebMarkupContainer(days.newChildId());
-			days.add(day);
-			
-			// Periods
-			RepeatingView periodsView = new RepeatingView("periods"); //$NON-NLS-1$
-			day.add(periodsView);
-			
-			for (Period period : datePeriod.getPeriods()) {
-				WebMarkupContainer periodCell = new WebMarkupContainer(periodsView.newChildId());
-				periodsView.add(periodCell);
-				
-				periodCell.add(createPeriodDateComponent("period", period, date));
+			if (weekHeader != null) {
+				weekHeader.add(new SimpleAttributeModifier("colspan", ""+daysInWeek)); //$NON-NLS-1$ //$NON-NLS-2$
 			}
 			
-			daysInWeek++;
+					
+			// Navigation
+			Link backLink = createBackLink("backLink", firstWeekDate.minusWeeks(periods.calculateNumberOfWeeks(firstWeekDate.minusDays(1).toDate(), 7)));
+			backLink.add(new Image("backIcon", Icons.LEFT)); //$NON-NLS-1$ //$NON-NLS-2$
+			calendar.add(backLink);
+			
+			Link forwardLink = createForwardLink("forwardLink", weekDate);
+			forwardLink.add(new Image("forwardIcon", Icons.RIGHT)); //$NON-NLS-1$ //$NON-NLS-2$
+			calendar.add(forwardLink);
 		}
-		
-		if (weekHeader != null) {
-			weekHeader.add(new SimpleAttributeModifier("colspan", ""+daysInWeek)); //$NON-NLS-1$ //$NON-NLS-2$
-		}
-		
-				
-		// Navigation
-		Link backLink = createBackLink("backLink", firstWeekDate.minusWeeks(periods.calculateNumberOfWeeks(firstWeekDate.minusDays(1).toDate(), 7)));
-		backLink.add(new Image("backIcon", Icons.LEFT)); //$NON-NLS-1$ //$NON-NLS-2$
-		add(backLink);
-		
-		Link forwardLink = createForwardLink("forwardLink", weekDate);
-		forwardLink.add(new Image("forwardIcon", Icons.RIGHT)); //$NON-NLS-1$ //$NON-NLS-2$
-		add(forwardLink);
 	}
 	
 	protected abstract Link createBackLink(String wicketId, DateMidnight previousWeekDate);
