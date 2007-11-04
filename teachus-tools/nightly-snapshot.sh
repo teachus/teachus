1fc623643750152f4252b650c650762d4bc5d70f
Frank Bille <frank@teachus.dk>
2007-11-04 21:55:38 +0000

    Removed log output again. If someone wants to grep the log they can do it on this script instead.
    git-svn-id: http://svn.teachus.dk/dk.teachus/trunk@477 9534b298-6716-0410-9c14-e4ca5a2019ab

--- teachus-tools/nightly-snapshot.sh
+++ teachus-tools/nightly-snapshot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-svn up > target/nightly-snapshot.log
-mvn clean compile exec:java -Dexec.mainClass="dk.teachus.tools.NightlySnapshot" >> target/nightly-snapshot.log
+svn up
+mvn clean compile exec:java -Dexec.mainClass="dk.teachus.tools.NightlySnapshot"
