5e141e402df74cbaf1b373c244723a68df97e8b9
Frank Bille <frank@teachus.dk>
2008-01-13 11:36:05 +0000

    TEACHUS-39: Externalized the filter button labels
    git-svn-id: http://svn.teachus.dk/dk.teachus/trunk@510 9534b298-6716-0410-9c14-e4ca5a2019ab

--- teachus-frontend/src/main/java/dk/teachus/frontend/components/list/FilterSubmitToolbar.java
+++ teachus-frontend/src/main/java/dk/teachus/frontend/components/list/FilterSubmitToolbar.java
@@ -8,23 +8,25 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.model.Model;
 
+import dk.teachus.frontend.TeachUsSession;
+
 public class FilterSubmitToolbar extends AbstractToolbar {
 	private static final long serialVersionUID = 1L;
 
 	public FilterSubmitToolbar(DataTable table, final TeachUsFilter filter) {
 		super(table);
 		
-		WebMarkupContainer td = new WebMarkupContainer("td");
+		WebMarkupContainer td = new WebMarkupContainer("td"); //$NON-NLS-1$
 		add(td);
 		
-		td.add(new AttributeModifier("colspan", true, new Model(String
+		td.add(new AttributeModifier("colspan", true, new Model(String //$NON-NLS-1$
 				.valueOf(table.getColumns().length))));
 		
-		Button filterSubmit = new Button("filterSubmit");
-		filterSubmit.add(new SimpleAttributeModifier("value", "Filter"));
+		Button filterSubmit = new Button("filterSubmit"); //$NON-NLS-1$
+		filterSubmit.add(new SimpleAttributeModifier("value", TeachUsSession.get().getString("FilterSubmitToolbar.filter"))); //$NON-NLS-1$ //$NON-NLS-2$
 		td.add(filterSubmit);
 		
-		Button filterReset = new Button("filterReset") {
+		Button filterReset = new Button("filterReset") { //$NON-NLS-1$
 			private static final long serialVersionUID = 1L;
 
 			@Override
@@ -32,7 +34,7 @@ public class FilterSubmitToolbar extends AbstractToolbar {
 				filter.reset();
 			}
 		};
-		filterReset.add(new SimpleAttributeModifier("value", "Show all"));
+		filterReset.add(new SimpleAttributeModifier("value", TeachUsSession.get().getString("FilterSubmitToolbar.showAll"))); //$NON-NLS-1$ //$NON-NLS-2$
 		td.add(filterReset);
 	}
 	
