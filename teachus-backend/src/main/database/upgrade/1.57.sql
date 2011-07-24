4984b44edc73e5e6a1671bfea04508e9912289b0
Frank Bille <frank@teachus.dk>
2011-07-24 12:36:10 +0200

    Removed the timezone columns from the mysql database. Both from the standard script as well as from any existing database.

--- teachus-backend/src/main/database/upgrade/1.57.sql
+++ teachus-backend/src/main/database/upgrade/1.57.sql
@@ -1,3 +1,17 @@
+-- Drop timezone columns
+ALTER TABLE
+	booking
+		DROP COLUMN date_tz,
+		DROP COLUMN create_date_tz,
+		DROP COLUMN update_date_tz;
+		
+ALTER TABLE
+	period
+		DROP COLUMN begin_date_tz,
+		DROP COLUMN end_date_tz,
+		DROP COLUMN start_time_tz,
+		DROP COLUMN end_time_tz;
+
 -- Upgrade version
 UPDATE
 	application_configuration 
